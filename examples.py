689053ce4352521d38533c4a9a84e8dc65e3da15
LLaCorte <lorenzolacorte99@gmail.com>
2024-07-04 18:20:08 +0900

    Add and Refactor Examples

--- examples.py
+++ examples.py
@@ -36,6 +36,196 @@ from logging_utilities import (
     log_init, log_params, log_finish, create_iter_kwargs, save_data)
 from utility_functions import secret_key_rate
 
+from utility_functions import pmf_to_cdf
+from matplotlib.ticker import MaxNLocator
+import itertools
+
+
+def remove_unstable_werner(pmf, w_func, threshold=1.0e-15):
+    """
+    Removes unstable Werner parameters where the probability mass is below a specified threshold
+    and returns a new Werner parameter array without modifying the input array.
+    
+    Parameters:
+    - pmf (np.array): The probability mass function array.
+    - w_func (np.array): The input Werner parameter array.
+    - threshold (float): The threshold below which Werner parameters are considered unstable.
+    
+    Returns:
+    - np.array: A new Werner parameter array with unstable parameters removed.
+    """
+    new_w_func = w_func.copy()
+    for t in range(len(pmf)):
+        if pmf[t] < threshold:
+            new_w_func[t] = np.nan
+    return new_w_func
+
+
+def entanglement_generation(p_gen=0.5, t_trunc=20):
+    parameters = {
+        # A protocol is represented by a tuple of 0 and 1,
+        # where 0 stands for swap and 1 stands for distillation.
+        # This example is a 0-level swap protocol,
+        # as we consider only the entanglement generation.
+        "protocol": (),
+        # success probability of entanglement generation
+        "p_gen": p_gen,
+        # truncation time for the repeater scheme.
+        # It should be increased to cover more time step
+        # if the success proability decreases.
+        # Commercial hardware can easily handle up to t_trunc=1e5
+        "t_trunc": t_trunc,
+        "w0": 0.98, # ignore this for the sake of the example
+        "p_swap": 0.5  # ignore this as well
+    }
+    pmf, _ = repeater_sim(parameters)
+    return pmf
+
+
+def plot_entanglement_generation():
+    t_trunc = 20
+    p_gens = [0.2, 0.5]
+    
+    fig, axs = plt.subplots(2, 2, figsize=(12.8, 7.2))    
+
+    for i, p_gen in enumerate(p_gens):
+        pmf = entanglement_generation(p_gen, t_trunc)
+        pmf = pmf[:t_trunc]
+        cdf = np.cumsum(pmf)
+
+        axs[i][0].set_title("CDF")
+        axs[i][0].plot(np.arange(t_trunc), cdf)
+        axs[i][0].set_xlabel("Waiting time $T$")
+        axs[i][0].set_ylabel("Probability")
+        axs[i][0].xaxis.set_major_locator(MaxNLocator(integer=True))
+        axs[i][0].set_ylim(0, 1)
+
+        axs[i][1].set_title("PMF")
+        axs[i][1].plot(np.arange(t_trunc), pmf)
+        axs[i][1].set_xlabel("Waiting time $T$")
+        axs[i][1].set_ylabel("Probability")
+        axs[i][1].xaxis.set_major_locator(MaxNLocator(integer=True))
+        axs[i][1].set_ylim(0, 1)
+
+    fig.suptitle(f'Entanglement Generation for p_gen: {", ".join([str(p_gen) for p_gen in p_gens])}, t_trunc: {t_trunc}')
+
+    plt.tight_layout()
+    plt.subplots_adjust(wspace=0.2)
+
+    fig.savefig(f"gen_example_{t_trunc}.png")
+
+
+def entanglement_swap(p_gen=0.5, p_swap=0.5, w_0=0.99, t_trunc=20):
+    parameters = {
+        # A protocol is represented by a tuple of 0 and 1,
+        # where 0 stands for swap and 1 stands for distillation.
+        # This example is a 3-level swap protocol, without distillation
+        # spanning over 9 nodes (i.e. 8 segments)
+        "protocol": (0, 0, 0),
+        # success probability of entanglement generation
+        "p_gen": p_gen,
+        # success probability of entanglement swap
+        "p_swap": p_swap,
+        # truncation time for the repeater scheme.
+        # It should be increased to cover more time step
+        # if the success proability decreases.
+        # Commercial hardware can easily handle up to t_trunc=1e5
+        "t_trunc": t_trunc,
+        "w0": w_0,
+        # the memory coherence time,
+        # in the unit of one attempt of elementary link generation.
+        "t_coh": 400,
+    }
+    pmf, w_func = repeater_sim(parameters)
+    
+    t = 0
+    # Remove unstable Werner parameter,
+    # because the the probability mass is too low 10^(-22)
+    while(pmf[t] < 1.0e-17):
+        w_func[t] = np.nan
+        t += 1
+
+    return pmf, w_func
+
+
+def entanglement_dist(p_gen=0.5, p_swap=0.5, w_0=0.99, t_trunc=20):
+    parameters = {
+        # A protocol is represented by a tuple of 0 and 1,
+        # where 0 stands for swap and 1 stands for distillation.
+        # This example is a 2-level swap protocol
+        # - 0-level: entanglement generation
+        # - 1-level: entanglement distillation of 2 links into a higher quality one
+        # - 2,3,4-level: entanglement swapping
+        # It spans over 5 nodes (i.e. 4 segments)
+        "protocol": (1, 0, 0, 0),
+        "p_gen": p_gen,
+        "p_swap": p_swap,
+        "w0": w_0,
+        "t_coh": 400,
+        "t_trunc": t_trunc,
+    }
+
+    pmf, w_func = repeater_sim(parameters)
+    t = 0
+    # Remove unstable Werner parameter,
+    # because the the probability mass is too low 10^(-22)
+    while(pmf[t] < 1.0e-17):
+        w_func[t] = np.nan
+        t += 1
+    return pmf, w_func
+
+
+def plot_protocol_func(protocol_func=entanglement_swap):
+    t_trunc = 30000
+
+    sim_params = [
+        {"p_gen": 0.1, "p_swap": 0.1, "w_0": 0.9, "t_trunc": t_trunc},
+        {"p_gen": 0.1, "p_swap": 0.25, "w_0": 0.9, "t_trunc": t_trunc},
+    ]
+
+    fig, axs = plt.subplots(1, 3, figsize=(15, 7.2))
+    
+    handles, labels = [], []
+    for i, sim_param in enumerate(sim_params):
+        pmf, w_func = protocol_func(**sim_param)
+
+        w_func[0] = np.nan
+        threshold = 1.0e-15
+        for idx, t in enumerate(pmf):
+            if t < threshold:
+                w_func[idx] = np.nan
+
+        pmf, w_func = pmf[:t_trunc], w_func[:t_trunc]
+        cdf = np.cumsum(pmf)
+
+        plot_configs = [
+            ("PMF", pmf, "Probability"),
+            ("CDF", cdf, "Probability"),
+            ("Werner", w_func, "Werner parameter")
+        ]
+
+        for j, (title, data, ylabel) in enumerate(plot_configs):
+            axs[j].set_title(title)
+            axs[j].plot(np.arange(t_trunc), data, label=title)
+            axs[j].set_xlabel("Waiting time $T$")
+            axs[j].set_ylabel(ylabel)
+            axs[j].xaxis.set_major_locator(MaxNLocator(integer=True))
+        
+        handles.append(axs[0].lines[i])
+        labels.append(f"{sim_param}")
+
+
+    if "dist" in protocol_func.__name__:
+        plt.suptitle(f"Distillation example")
+    else:
+        plt.suptitle(f"Swap example")
+    
+    plt.tight_layout()
+    plt.subplots_adjust(wspace=0.25, hspace=0.5)
+
+    fig.legend(handles, labels)
+    fig.savefig(f"{protocol_func.__name__}_example_{t_trunc}.png")
+
 
 def swap_protocol():
     """
@@ -47,7 +237,7 @@ def swap_protocol():
     parameters = {
         # A protocol is represented by a tuple of 0 and 1,
         # where 0 stands for swap and 1 stands for distillation.
-        # This example is a 3-level swap protocol,
+        # This example is a 3-level swap protocol (without distillation)
         # spanning over 9 nodes (i.e. 8 segments)
         "protocol": (0, 0, 0),
         # success probability of entanglement generation
@@ -57,7 +247,7 @@ def swap_protocol():
         # initial Werner parameter
         "w0": 0.98,
         # memory cut-off time
-        "cutoff": (16, 31, 55),
+        # "cutoff": (16, 31, 55),
         # the memory coherence time,
         # in the unit of one attempt of elementary link generation.
         "t_coh": 400,
@@ -97,12 +287,11 @@ def swap_protocol():
     # Run the calculation
     pmf, w_func = repeater_sim(parameters)
     end = time.time()
-    t = 0
+
     # Remove unstable Werner parameter,
     # because the the probability mass is too low 10^(-22)
-    while(pmf[t] < 1.0e-17):
-        w_func[t] = np.nan
-        t += 1
+    w_func = remove_unstable_werner(pmf, w_func)
+
     print("Elapse time\n", end-start)
     print()
     plot_algorithm(pmf, w_func, axs, t_trunc=2000)
@@ -118,10 +307,10 @@ def swap_protocol():
             for j in range(2):
                 axs[i][j].legend(legend)
     plt.tight_layout()
-    fig.show()
+    fig.savefig("swap_protocol.png")
 
 
-def mixed_protocol():
+def mixed_protocol(cutoff=False):
     """
     Here we show a mixed protocol with the number of qubits and segments not
     a power of 2. Notice that it is only for demonstration purpose
@@ -142,13 +331,17 @@ def mixed_protocol():
         "p_swap": 0.5,
         "w0": 0.85,
         "t_coh": 400,
-        "t_trunc": 3000,
-        }
+        "t_trunc": 1200,
+    }
     p_gen = parameters["p_gen"]
     t_trunc = parameters["t_trunc"]
     w0 = parameters["w0"]
 
     simulator = RepeaterChainSimulation()
+
+    rows = 4
+    fig, axs = plt.subplots(rows, 3, figsize=(15, 4*rows), sharex=True)
+
     ############################
     # Part1
     # Generate entanglement link for all qubits pairs between AB, BC and CD
@@ -168,6 +361,16 @@ def mixed_protocol():
     pmf_span1_dist3, w_span1_dist3 = simulator.compute_unit(
         parameters, pmf_span1_dist2, w_span1_dist2,
         pmf_span1_dist1, w_span1_dist1, unit_kind="dist")
+    
+    cdf_span1_dist3 = pmf_to_cdf(pmf_span1_dist3)
+
+    axs[0][0].plot(np.arange(t_trunc), pmf_span1_dist3)
+    axs[0][0].set_title("PMF")
+    axs[0][1].plot(np.arange(t_trunc), cdf_span1_dist3)
+    axs[0][1].set_title("CDF")
+    axs[0][2].plot(np.arange(t_trunc), remove_unstable_werner(pmf_span1_dist3, w_span1_dist3))
+    axs[0][2].set_title("Werner parameter")
+    axs[0][2].set_ylim([0, 1])
 
     ############################
     # Part3: Among B, C and D. Performed simultaneously as part2
@@ -175,6 +378,15 @@ def mixed_protocol():
     # We begin from swap between B-C and C-D, for all 3 pairs of elementary link.
     pmf_span2_dist1, w_span2_dist1 = simulator.compute_unit(
         parameters, pmf_span1_dist1, w_span1_dist1, unit_kind="swap")
+
+    cdf_span2_dist1 = pmf_to_cdf(pmf_span2_dist1)
+    
+    axs[1][0].plot(np.arange(t_trunc), pmf_span2_dist1)
+    axs[1][1].plot(np.arange(t_trunc), cdf_span2_dist1)
+    axs[1][2].plot(np.arange(t_trunc), remove_unstable_werner(pmf_span2_dist1, w_span2_dist1))
+    axs[1][2].set_ylim([0, 1])
+
+    print(f"Werner parameter at time 500 before distillation: {w_span2_dist1[500]}") # DEBUG
 
     # When B1-D1, B2-D2 and prepared, we distill them
     pmf_span2_dist2, w_span2_dist2 = simulator.compute_unit(
@@ -184,26 +396,94 @@ def mixed_protocol():
     # a single link between B and D
     # Here we add a cutoff on the memory storage time to increase the fidelity,
     # at the cost of a longer waiting time.
-    parameters["cutoff"] = 50
     pmf_span2_dist3, w_span2_dist3 = simulator.compute_unit(
         parameters, pmf_span2_dist2, w_span2_dist2,
         pmf_span2_dist1, w_span2_dist1, unit_kind="dist")
-    del parameters["cutoff"]
+
+    cdf_span2_dist3 = pmf_to_cdf(pmf_span2_dist3)
+    axs[2][0].plot(np.arange(t_trunc), pmf_span2_dist3)
+    axs[2][1].plot(np.arange(t_trunc), cdf_span2_dist3)
+    axs[2][2].plot(np.arange(t_trunc), remove_unstable_werner(pmf_span2_dist3, w_span2_dist3), label='Without cutoff')
+    axs[2][2].set_ylim([0, 1])
+
+    print(f"Werner parameter at time 500 after distillation: {w_span2_dist3[500]}") # DEBUG
+
+    if cutoff:
+        parameters["cutoff"] = 50
+        pmf_span2_dist3_cut, w_span2_dist3_cut = simulator.compute_unit(
+            parameters, pmf_span2_dist2, w_span2_dist2,
+            pmf_span2_dist1, w_span2_dist1, unit_kind="dist")
+        del parameters["cutoff"]
+
+        cdf_span2_dist3_cut = pmf_to_cdf(pmf_span2_dist3_cut)
+        axs[2][0].plot(np.arange(t_trunc), pmf_span2_dist3_cut)
+        axs[2][1].plot(np.arange(t_trunc), cdf_span2_dist3_cut)
+        axs[2][2].plot(np.arange(t_trunc), remove_unstable_werner(pmf_span2_dist3_cut, w_span2_dist3_cut), label='With cutoff')
 
     ############################
     # Part4
     # We connect A-B and B-D with a swap
-    parameters["cutoff"] = 50
     pmf_span3_dist3, w_span3_dist3 = simulator.compute_unit(
         parameters, pmf_span1_dist3, w_span1_dist3,
         pmf_span2_dist3, w_span2_dist3, unit_kind="swap")
-    del parameters["cutoff"]
+
+    cdf_span3_dist3 = pmf_to_cdf(pmf_span3_dist3)
+    axs[3][0].plot(np.arange(t_trunc), pmf_span3_dist3)
+    axs[3][1].plot(np.arange(t_trunc), cdf_span3_dist3)
+    axs[3][2].plot(np.arange(t_trunc), remove_unstable_werner(pmf_span3_dist3, w_span3_dist3), label='Without cutoff')
+    axs[3][2].set_ylim([0, 1])
+
+    if cutoff:
+        parameters["cutoff"] = 50
+        pmf_span3_dist3_cut, w_span3_dist3_cut = simulator.compute_unit(
+            parameters, pmf_span1_dist3, w_span1_dist3,
+            pmf_span2_dist3_cut, w_span2_dist3_cut, unit_kind="swap")
+        del parameters["cutoff"]
+
+        cdf_span3_dist3_cut = pmf_to_cdf(pmf_span3_dist3_cut)
+        axs[3][0].plot(np.arange(t_trunc), pmf_span3_dist3_cut)
+        axs[3][1].plot(np.arange(t_trunc), cdf_span3_dist3_cut)
+        axs[3][2].plot(np.arange(t_trunc), remove_unstable_werner(pmf_span3_dist3_cut, w_span3_dist3_cut), label='With cutoff')
+        axs[3][2].set_ylim([0, 1])
 
     print("secret key rate", secret_key_rate(pmf_span3_dist3, w_span3_dist3))
-    # Let's plot the final time and Werner parameter distribution
-    fig, axs = plt.subplots(2, 2)
-    plot_algorithm(pmf_span3_dist3, w_span3_dist3, axs)
-    fig.show()
+    if cutoff:
+        print("secret key rate with cutoffs", secret_key_rate(pmf_span3_dist3_cut, w_span3_dist3_cut))
+
+
+    plt.tight_layout()
+    plt.subplots_adjust(left=0.1, hspace=0.1)
+
+    if cutoff:
+        axs[2][2].legend()
+        axs[3][2].legend()
+    
+    row_titles = ["2 Distillations of A-B", "Swapping B-C-D to B-D", "2 Distillations of B-D", "Swapping A-B-D to A-D"]
+    for ax, row_title in zip(axs[:,0], row_titles):
+        ax.text(-0.2, 0.5, row_title, transform=ax.transAxes, ha="right", va="center", rotation=90, fontsize=14)
+
+    fig.savefig(f"mixed_protocol_t_trunc{t_trunc}.png")
+
+    # Plot with only final results
+    fig2, axs2 = plt.subplots(1, 3, figsize=(15, 4), sharex=True)
+
+    axs2[0].plot(np.arange(t_trunc), pmf_span3_dist3)
+    axs2[0].set_title("PMF")
+    axs2[1].plot(np.arange(t_trunc), cdf_span3_dist3)
+    axs2[1].set_title("CDF")
+    axs2[2].plot(np.arange(t_trunc), remove_unstable_werner(pmf_span3_dist3, w_span3_dist3), label='Without cutoff')
+    axs2[2].set_title("Werner parameter")
+    axs2[2].set_ylim([0, 1])
+
+    if cutoff:
+        axs2[0].plot(np.arange(t_trunc), pmf_span3_dist3_cut)
+        axs2[1].plot(np.arange(t_trunc), cdf_span3_dist3_cut)
+        axs2[2].plot(np.arange(t_trunc), remove_unstable_werner(pmf_span3_dist3_cut, w_span3_dist3_cut), label='With cutoff')
+        axs2[2].legend()
+
+    plt.tight_layout()
+    plt.subplots_adjust(left=0.1, hspace=0.1)
+    fig2.savefig(f"mixed_protocol_final_t_trunc{t_trunc}.png")
 
 
 def optimize_cutoff_time():
@@ -255,5 +535,101 @@ def optimize_cutoff_time():
     logging.info("Secret key rate without cut-off: {:.6f}".format(key_rate))
     logging.info("Rate without truncation time: {}\n".format(key_rate))
 
-if __name__ == "__main__":  #  run the example
+
+def no_trunc_swap():
+    parameters = {
+        "protocol": (),
+        "p_gen": 0.1,
+        "p_swap": 0.5,
+        "w0": 0.98,
+        "t_coh": 400,
+        "t_trunc": None,
+        "cut_type": "memory_time",
+    }
+
+    pmf, w_func = repeater_sim(parameters)
+    t = 0
+    # Remove unstable Werner parameter,
+    # because the the probability mass is too low 10^(-22)
+    while(pmf[t] < 1.0e-17):
+        w_func[t] = np.nan
+        t += 1
+
+    t_trunc = len(pmf)
+    fig, axs = plt.subplots(2, 2)
+    plot_algorithm(pmf, w_func, axs, t_trunc)
+    print("secret key rate", secret_key_rate(pmf, w_func))
+
+    # plot
+    legend = None
+    axs[0][0].set_title("CDF")
+    axs[0][1].set_title("PMF")
+    axs[1][0].set_title("Werner")
+    if legend is not None:
+        for i in range(2):
+            for j in range(2):
+                axs[i][j].legend(legend)
+    plt.tight_layout()
+    fig.savefig("swap_protocol.png")
+
+
+'''
+Get in input a set of parameters and return the truncation time to cover 99.7% of the distribution
+'''
+def trunc_experiment(parameters):
+    trunc_to_coverage = {}
+    
+    levels = len(parameters["protocol"]) if isinstance(parameters["protocol"], (tuple)) else 1
+    unit = int((2/parameters["p_swap"])**(levels) / parameters["p_gen"])
+    
+    t_trunc = unit
+    while True:
+        parameters["t_trunc"] = t_trunc
+        parameters["t_coh"] = t_trunc/100
+        pmf, _ = repeater_sim(parameters)
+
+        coverage = pmf_to_cdf(pmf)[-1]
+        t_trunc += unit
+
+        trunc_to_coverage[t_trunc] = coverage
+
+        if coverage > 0.997:
+            return trunc_to_coverage 
+
+def plot_trunc_to_coverage(trunc_to_coverage, parameters):
+    plt.figure(figsize=(10, 6))
+
+    lists = sorted(trunc_to_coverage.items())
+    x, y = zip(*lists)
+    plt.plot(x, y, marker='o')
+    plt.title('Time to Coverage')
+    plt.xlabel('Truncation Time')
+    plt.ylabel('Coverage')
+    
+    filename = f"{len(parameters['protocol'])}level_gen{parameters['p_gen']}_swap{parameters['p_swap']}.png"
+    plt.savefig(filename)
+
+def run_trunc_experiments():
+    protocols = [(), (0,), (0, 0), (0, 0, 0), (0, 0, 0, 0)]
+    p_gens = [0.1, 0.01]
+    p_swaps = [0.1, 0.01]
+
+    all_combinations = itertools.product(protocols, p_gens, p_swaps)
+
+    for protocol, p_gen, p_swap in all_combinations:
+        parameters = {
+            "protocol": protocol,
+            "p_gen": p_gen,
+            "p_swap": p_swap,
+            "w0": 0.98,
+        }
+        trunc_to_coverage = trunc_experiment(parameters)
+        plot_trunc_to_coverage(trunc_to_coverage, parameters)
+
+if __name__ == "__main__":
+    # swap_protocol()
     mixed_protocol()
+    # optimize_cutoff_time()
+    # plot_protocol_func(protocol_func=entanglement_swap)
+    # plot_protocol_func(protocol_func=entanglement_dist)
+    # run_trunc_experiments()
