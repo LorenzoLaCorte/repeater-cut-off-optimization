43367ad71649c881697291e208a7a30e8d64a232
LLaCorte <lorenzolacorte99@gmail.com>
2024-08-02 15:15:34 +0900

    Add results

--- distillation_results.sh
+++ distillation_results.sh
@@ -2,20 +2,40 @@
 
 MAX_SWAPS=3
 MAX_DISTS=7
-OPTIMIZER="gp"
-SPACE="enumerate"
-FILENAME="output_${OPTIMIZER}_${SPACE}.txt"
-TMPFILE=$(mktemp)
+OPTIMIZER_SPACE_COMBS=(
+    "gp strategy 120 0.9 0.9 0.867"
+    "bf enumerate 120 0.9 0.9 0.867"
+    "gp strategy 10000 0.5 0.5 0.9"
+    "bf enumerate 10000 0.5 0.5 0.9"
+)
 
-{ time py distillation_gp.py \
-    --max_swaps="$MAX_SWAPS" \
-    --max_dists="$MAX_DISTS" \
-    --optimizer="$OPTIMIZER" \
-    --space="$SPACE" \
-    --filename="$FILENAME"; } 2>> "$TMPFILE"
+for TUPLE in "${OPTIMIZER_SPACE_COMBS[@]}"; do
+    OPTIMIZER=$(echo $TUPLE | awk '{print $1}')
+    SPACE=$(echo $TUPLE | awk '{print $2}')
+    T_COH=$(echo $TUPLE | awk '{print $3}')
+    P_GEN=$(echo $TUPLE | awk '{print $4}')
+    P_SWAP=$(echo $TUPLE | awk '{print $5}')
+    W0=$(echo $TUPLE | awk '{print $6}')
+    
+    FILENAME="output_${OPTIMIZER}_${SPACE}_tcoh${T_COH}_pgen${P_GEN}_pswap${P_SWAP}_w0${W0}.txt"
+    TMPFILE=$(mktemp)
+    
+    echo "Running distillation with optimizer $OPTIMIZER and space $SPACE..."
 
-# Extract the time taken and append it to the output file
-echo "Time taken:" >> "$FILENAME"
-tail -n 3 "$TMPFILE" >> "$FILENAME"
+    { time python distillation_gp.py \
+        --max_swaps="$MAX_SWAPS" \
+        --max_dists="$MAX_DISTS" \
+        --optimizer="$OPTIMIZER" \
+        --space="$SPACE" \
+        --filename="$FILENAME" \
+        --t_coh "${T_COH[@]}" \
+        --p_gen "${P_GEN[@]}" \
+        --p_swap "${P_SWAP[@]}" \
+        --w0 "${W0[@]}"; } 2>&1 | tee -a "$TMPFILE"
 
-rm "$TMPFILE"
+    # Extract the time taken and append it to the output file
+    echo "Time taken:" >> "$FILENAME"
+    tail -n 3 "$TMPFILE" >> "$FILENAME"
+
+    rm "$TMPFILE"
+done
